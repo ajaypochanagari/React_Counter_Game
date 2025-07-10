d9e7756a5141099f56f53d4e2000af6072005797
Ajay Goud <105651124+ajaypochanagari@users.noreply.github.com>
2025-07-10 10:19:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+  const[count,setCount]=useState(0);
+  return (
+    <div className="App">
+      <h1>Counter Value: {count}</h1>
+      <button  onClick={()=>setCount(count+1)}>Increment</button>
+      <button onClick={()=>setCount(count-1)}>Decrement</button>
+      
+    </div>
+  );
+}
+
+export default App;
